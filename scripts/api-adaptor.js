dffbc755585fab27d232a7154679e8a38e61f953
simonhazelgrove <simonhazelgrove@hotmail.com>
2017-09-19 16:06:09 +1000

    Now displaying fixtures

--- scripts/api-adaptor.js
+++ scripts/api-adaptor.js
@@ -33,29 +33,87 @@ function GetApiAdaptor() {
         joinData: function (success) {
             var self = this;
             var data = {
-                id: "",
-                name: "",
-                week: 0,
-                table: null
+                id: self.apiCompetition.id,
+                name: self.apiCompetition.caption,
+                currentWeek: self.apiCompetition.currentMatchday,
+                teams: _.map(self.apiTeams.teams, self.convertApiTeam),
+                fixtures: _.map(self.apiFixtures.fixtures, self.convertApiFixture),
+                table: _.map(self.apiLeagueTable.standing, self.convertApiTableRow),
+                thisWeeksFixtures: null
             };
-            data.id = self.apiCompetition.id;
-            data.name = self.apiCompetition.caption;
-            data.week = self.apiCompetition.currentMatchday;
-            data.table = _.map(self.apiLeagueTable.standing, self.convertApiTableRow);
-
+            self.createTeamStreaks(data);
+            self.createTableTeams(data);
+            self.createFixtureTeams(data);
+            data.thisWeeksFixtures = _.where(data.fixtures, { week: data.currentWeek });
             success(data);
         },
+        convertApiTeam: function (apiTeam, key) {
+            var team = {
+                id: key,
+                name: apiTeam.name,
+                homeStreak: null,
+                awayStreak: null
+            }
+            return team;
+        },
+        convertApiFixture: function (apiFixture, key) {
+            var fixture = {
+                id: key,
+                date: apiFixture.date,
+                week: apiFixture.matchday,
+                completed: apiFixture.status == "FINISHED",
+                homeTeam: null,
+                homeTeamName: apiFixture.homeTeamName,
+                homeTeamScore: apiFixture.result.goalsHomeTeam,
+                awayTeam: null,
+                awayTeamName: apiFixture.awayTeamName,
+                awayTeamScore: apiFixture.result.goalsAwayTeam,
+            }
+            return fixture;
+        },
         convertApiTableRow: function (apiTableRow, key) {
-            var teamRow = {
+            var tableRow = {
                 row: key + 1,
-                teamId: apiTableRow.teamId,
                 teamName: apiTableRow.teamName,
                 team: null,
                 playedGames: apiTableRow.playedGames,
                 goalDifference: apiTableRow.goalDifference,
                 points: apiTableRow.points
             }
-            return teamRow;
+            return tableRow;
+        },
+        createTeamStreaks: function (data) {
+            var self = this;
+            _.each(data.teams, function(team) {
+                var homeResults = _.chain(data.fixtures)
+                    .filter(function(fixture) { return fixture.homeTeamName == team.name && fixture.completed == true; })
+                    .sortBy("week")
+                    .value();
+                var awayResults = _.chain(data.fixtures)
+                    .filter(function(fixture) { return fixture.awayTeamName == team.name && fixture.completed == true; })
+                    .sortBy("week")
+                    .value();
+                team.homeStreak = self.getStreakFromFixtures(homeResults);
+                team.awayStreak = self.getStreakFromFixtures(awayResults);
+            }, self)
+        },
+        getStreakFromFixtures: function(fixtures) {
+            var streak = {
+                type: "W",
+                length: 0
+            };
+            return streak;
+        },
+        createTableTeams: function(data) {
+            _.each(data.table, function (tableRow) {
+                tableRow.team = _.findWhere(data.teams, { name: tableRow.teamName });
+            })
+        },
+        createFixtureTeams: function(data) {
+            _.each(data.fixtures, function (fixture) {
+                fixture.homeTeam = _.findWhere(data.teams, { name: fixture.homeTeamName });
+                fixture.awayTeam = _.findWhere(data.teams, { name: fixture.awayTeamName });
+            })
         }
     }
 }
--- scripts/api-display.js
+++ scripts/api-display.js
@@ -2,16 +2,39 @@ function GetApiDisplay(apiAdaptor) {
     return {
         apiAdaptor: apiAdaptor,
         tableElement: $("#league-table"),
+        fixturesElement: $("#league-fixtures"),
         displayLeague: function () {
             var self = this;
-            this.apiAdaptor.getData(function(data) {
-                var tableHtml = "<table><tr><th>Pos</th><th>Team</th><th>Played</th><th>Diff</th><th>Points</th></tr>";
-                _.forEach(data.table, function (tableItem) {
-                    tableHtml += "<tr><td>" + tableItem.row + "</td><td>" + tableItem.teamName + "</td><td>" + tableItem.playedGames + "</td><td>" + tableItem.goalDifference + "</td><td>" + tableItem.points + "</td></tr>";
-                });
-                tableHtml += "</table>";
-                self.tableElement.append(tableHtml);
+            this.apiAdaptor.getData(function (data) {
+                self.drawFixtures(data);
+                self.drawTable(data);
             });
+        },
+        drawFixtures: function (data) {
+            var self = this;
+            var fixtureHtml = ""
+            _.each(data.thisWeeksFixtures, function (fixture) {
+                fixtureHtml += "<div>";
+                fixtureHtml += fixture.homeTeam.name + " v " + fixture.awayTeam.name;
+                fixtureHtml += "</div>";
+            });
+            self.fixturesElement.append(fixtureHtml);
+        },
+        drawTable: function(data) {
+            var self = this;
+            var tableHtml = "<table><tr><th>Pos</th><th>Team</th><th>Played</th><th>Home Streak</th><th>Away Streak</th><th>Diff</th><th>Points</th></tr>";
+            _.each(data.table, function (tableItem) {
+                tableHtml += "<tr><td>" +
+                    tableItem.row + "</td><td>" +
+                    tableItem.teamName + "</td><td>" +
+                    tableItem.playedGames + "</td><td>" +
+                    tableItem.team.homeStreak.type + tableItem.team.homeStreak.length + "</td><td>" +
+                    tableItem.team.awayStreak.type + tableItem.team.awayStreak.length + "</td><td>" +
+                    tableItem.goalDifference + "</td><td>" +
+                    tableItem.points + "</td></tr>";
+            });
+            tableHtml += "</table>";
+            self.tableElement.append(tableHtml);
         }
     }
 }
